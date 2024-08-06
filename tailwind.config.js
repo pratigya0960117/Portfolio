4aecf9a3cb98cb20fab50eb54729a8f00cd33a9d
Your Name <your.email@example.com>
2024-08-06 20:13:28 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+tailwind.config = {
+    theme: {
+        extend:{
+            gridTemplateColumns: {
+                'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+              },
+              fontFamily:{
+                Outfit: ["Outfit","sans-serif"],
+                Ovo: ["Ovo","serif"]
+              },
+              animation:{
+                spin_slow: 'spin 6s linear infinite'
+              },
+              color:{
+                lightHover: '#fcf4ff',
+                darkHover: '#2a004a'
+              },
+              boxShadow:{
+                'black': '4px 4px 0 #000',
+                'white': '4px 4px 0 #fff',
+              },
+              colors:{
+                lightHover: '#fcf4ff',
+                darkHover: '2a004a',
+                darkTheme: '#11001F',
+              }
+              }
+
+        },
+        darkMode: 'selector'
+      
+    }
+
+  
+  
